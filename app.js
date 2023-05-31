9691e77142baf55705bb7c6604342347d8efde61
Marko Haapasaari <marko1927@student.hamk.fi>
2023-05-31 17:09:05 +0300

    configurations for deploying app on render

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const mongoose = require('mongoose')
 
 mongoose.connect(config.MONGODB_URI)
 
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-let PORT = process.env.PORT
+let PORT = process.env.PORT || 3001
 const MONGODB_URI = process.env.NODE_ENV === 'test' 
   ? process.env.TEST_MONGODB_URI
   : process.env.MONGODB_URI
